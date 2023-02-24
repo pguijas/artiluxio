9b80f177de83b97ab180ca0f85cd985b2740eab8
Ángel Miguélez <anxomm@gmail.com>
2023-02-24 20:05:46 +0100

    Preprocessing of images

--- lib/classifier.dart
+++ lib/classifier.dart
@@ -1,87 +1,71 @@
 import 'package:flutter/services.dart';
+import 'package:image/image.dart';
 
 // Import tflite_flutter
 import 'package:tflite_flutter/tflite_flutter.dart';
 
 class Classifier {
-  // name of the model file
-  final _modelFile = 'models/text_classification.tflite';
-  final _vocabFile = 'models/text_classification_vocab.txt';
 
-  // Maximum length of sentence
-  final int _sentenceLen = 256;
-
-  final String start = '<START>';
-  final String pad = '<PAD>';
-  final String unk = '<UNKNOWN>';
-
-  late Map<String, int> _dict;
+  final _predictorModel = "models/style_predict.tflite";
+  final _transferModel = "models/style_transfer.tflite";
+  final _outputFolder = "/storage/emulated/0/Download/";
 
   // TensorFlow Lite Interpreter object
-  late Interpreter _interpreter;
+  late Interpreter _predictorInterpreter;
+  late Interpreter _transferInterpreter;
 
   Classifier() {
     // Load model when the classifier is initialized.
     _loadModel();
-    _loadDictionary();
   }
 
   void _loadModel() async {
     // Creating the interpreter using Interpreter.fromAsset
-    _interpreter = await Interpreter.fromAsset(_modelFile);
+    _predictorInterpreter = await Interpreter.fromAsset(_predictorModel);
+    _transferInterpreter = await Interpreter.fromAsset(_transferModel);
     print('Interpreter loaded successfully');
   }
 
-  void _loadDictionary() async {
-    final vocab = await rootBundle.loadString('assets/$_vocabFile');
-    var dict = <String, int>{};
-    final vocabList = vocab.split('\n');
-    for (var i = 0; i < vocabList.length; i++) {
-      var entry = vocabList[i].trim().split(' ');
-      dict[entry[0]] = int.parse(entry[1]);
-    }
-    _dict = dict;
-    print('Dictionary loaded successfully');
+  Image _preprocess(Image image, height, width, saveAs) {
+
+    // Resize to the model input shape
+    Image transformed = copyResize(image, height: height, width: width);
+
+    // Normalize between [0,1]
+    transformed = transformed.convert(format: Format.float32);  // float32 for the models?
+
+    // Save the image
+    String outputFile = _outputFolder + saveAs;
+    encodeImageFile(outputFile, transformed);
+
+    return transformed;
+  }
+
+  void transfer(Image input, Image style) {
+    print("Preprocessing inputs...");
+    input = _preprocess(input, 384, 384, "input.jpg");
+    style = _preprocess(style, 256, 256, "style.jpg");
+
+    print("Allocating tensors...");
+    _predictorInterpreter.allocateTensors();
+
+    // if output tensor shape [1,1,1,100] and type is float32
+    var output = List.filled(1*1*1*100, 0).reshape([1,1,1,100]);
   }
 
   List<double> classify(String rawText) {
     // tokenizeInputText returns List<List<double>>
     // of shape [1, 256].
-    List<List<double>> input = tokenizeInputText(rawText);
+    //List<List<double>> input = tokenizeInputText(rawText);
 
     // output of shape [1,2].
     var output = List<double>.filled(2, 0).reshape([1, 2]);
 
     // The run method will run inference and
     // store the resulting values in output.
-    _interpreter.run(input, output);
+   // _predictorInterpreter.run(input, output);
 
     return [output[0][0], output[0][1]];
   }
 
-  List<List<double>> tokenizeInputText(String text) {
-    // Whitespace tokenization
-    final toks = text.split(' ');
-
-    // Create a list of length==_sentenceLen filled with the value <pad>
-    var vec = List<double>.filled(_sentenceLen, _dict[pad]!.toDouble());
-
-    var index = 0;
-    if (_dict.containsKey(start)) {
-      vec[index++] = _dict[start]!.toDouble();
-    }
-
-    // For each word in sentence find corresponding index in dict
-    for (var tok in toks) {
-      if (index > _sentenceLen) {
-        break;
-      }
-      vec[index++] = _dict.containsKey(tok)
-          ? _dict[tok]!.toDouble()
-          : _dict[unk]!.toDouble();
-    }
-
-    // returning List<List<double>> as our interpreter input tensor expects the shape, [1,256]
-    return [vec];
-  }
 }
